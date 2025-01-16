5780d62021335ec5dc38c9c530eacaf596fb2253
pavan8390 <pavanalandkar8390@gmail.com>
2025-01-16 16:24:09 +0530

    Initial commit

---
+++ backend/controller/product/uploadProduct.js
@@ -0,0 +1,47 @@
+const uploadProductPermission = require("../../helpers/permission");
+const productModel = require("../../models/productModel");
+
+async function UploadProductController(req, res) {
+    try {
+      const { productName, brandName, category, subcategory, productImage, price, sellingPrice, quantity, soldBy, percentOff, gst, gstAmount, sellerId } = req.body;
+  
+  
+      const productData = {
+        productName,
+        brandName,
+        category,
+        subcategory,
+        productImage,
+        price,
+        sellingPrice,
+        quantity,
+        soldBy,
+        percentOff,
+        gst,
+        gstAmount,
+        sellerId,
+      };
+  
+      console.log("Product data to save:", productData);
+  
+      const uploadProduct = new productModel(productData);
+      const saveProduct = await uploadProduct.save();
+  
+      res.status(201).json({
+        message: "Product uploaded successfully",
+        error: false,
+        success: true,
+        data: saveProduct,
+      });
+    } catch (err) {
+      res.status(400).json({
+        message: err.message || "An error occurred",
+        error: true,
+        success: false,
+      });
+    }
+}
+
+  
+
+module.exports = UploadProductController;
---
+++ backend/models/productModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema(
+  {
+    productName: String,
+    brandName: String,
+    category: String,
+    subcategory: String, // Subcategory field
+    productImage: [],
+    description: String,
+    price: Number,
+    percentOff : Number,
+    sellingPrice: Number,
+    quantity: Number,
+    gst: Number, // GST percentage
+    gstAmount: Number, // Calculated GST amount
+    soldBy: String,
+    features: String,
+    productInfo: String,
+    sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Seller', required: false }, // Optional field
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productModel = mongoose.model('product', productSchema);
+
+module.exports = productModel;
